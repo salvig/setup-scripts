e9eae3c5e403a9bfbf766a47e19e1fcdd4d358bf
salvig <salvatore.vigilante@icloud.com>
2018-10-21 15:01:20 +0200

    Adding script to build from source

---
+++ fromsrc/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+COIN_NAME=$1
+BLUE="\033[0;34m"
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m" 
+PURPLE="\033[0;35m"
+RED='\033[0;31m'
+GREEN="\033[0;32m"
+NC='\033[0m'
+MAG='\e[1;35m'
+
+declare COINS+=$(ls -1d */ | cut -d "/" -f1)
+echo $COINS | grep $1 >/dev/null 2>&1
+if [ $? -eq 0 ]
+ then . ./$1/$1.src
+ else echo "Such coin $1 is not supported"
+ echo "SUpported coins are: $(echo $COINS)"
+ exit 1
+fi
+
+build_coin() {
+cd
+git clone $GITHUB
+chmod -R 755 $GITREPO
+cd $GITREPO
+if [[ -d depends ]]
+	then cd depends
+	make NO_QT=1
+	cd ..
+	./autogen.sh
+	CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure --without-gui --disable-tests --prefix=$HOME/$COIN_NAME
+	make install-strip
+fi
+}
+
+create_package() {
+cd
+tar zcvf $COIN_NAME.tar.gz $COIN_NAME/
+mv $COIN_NAME.tar.gz $HOME/setup-scripts/fromsrc/$COIN_NAME
+cd $HOME/setup-scripts
+git pull
+git add .
+git commit -m "Added $COIN_NAME binaries package"
+git push
+}
+
+
+buil_coin
+create_packages
